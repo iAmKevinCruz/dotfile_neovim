c750699709064944d5f6649ea39313331e8aebf6
iAmKevinCruz <kcruz@varfaj.com>
2024-04-02 23:15:12 -0400

    Lualine tweak

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,6 +10,47 @@ return {
           -- component_separators = '|',
           -- section_separators = '',
         },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              file_status = true,      -- Displays file status (readonly status, modified status)
+              newfile_status = false,  -- Display new file status (new file means no write after created)
+              path = 4,                -- 0: Just the filename
+              -- 1: Relative path
+              -- 2: Absolute path
+              -- 3: Absolute path, with tilde as the home directory
+              -- 4: Filename and parent dir, with tilde as the home directory
+
+              shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+              -- for other components. (terrible name, any suggestions?)
+              symbols = {
+                modified = '●',      -- Text to show when the file is modified.
+                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                newfile = '[New]',     -- Text to show for newly created file before first write
+              }
+            },
+            -- lualine_x = {
+            --   {
+            --     'fileformat',
+            --     symbols = {
+            --       unix = '', -- e712
+            --       dos = '',  -- e70f
+            --       mac = '',  -- e711
+            --     },
+            --   },
+            --   {
+            --     'filetype',
+            --     colored = true,   -- Displays filetype icon in color if set to true
+            --     icon_only = false, -- Display only an icon for filetype
+            --     icon = { align = 'right' }, -- Display filetype icon on the right hand side
+            --     -- icon =    {'X', align='right'}
+            --     -- Icon string ^ in table is ignored in filetype component
+            --   },
+            -- },
+          }
+        },
         -- sections = {
         --   lualine_x = {
         --     -- {
