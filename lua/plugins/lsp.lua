9c06665868a7b54836854a5cef01ea4135ba06d4
iAmKevinCruz <kcruz@varfaj.com>
2024-04-15 12:06:32 -0400

    FEAT: Rust plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,156 +1,6 @@
 -- Keymaps
 
 return {
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  -- {
-  --   -- LSP Configuration & Plugins
-  --   'neovim/nvim-lspconfig',
-  --   dependencies = {
-  --     -- Automatically install LSPs to stdpath for neovim
-  --     'williamboman/mason.nvim',
-  --     'williamboman/mason-lspconfig.nvim',
-  --     -- { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-  --
-  --     -- Additional lua configuration, makes nvim stuff amazing!
-  --     'folke/neodev.nvim',
-  --       "hrsh7th/cmp-nvim-lsp",
-  --       "hrsh7th/cmp-buffer",
-  --       "hrsh7th/cmp-path",
-  --       "hrsh7th/cmp-cmdline",
-  --       "hrsh7th/nvim-cmp",
-  --       "L3MON4D3/LuaSnip",
-  --       "saadparwaiz1/cmp_luasnip",
-  --       "j-hui/fidget.nvim",
-  --   },
-  --   config = function()
-  --     local on_attach = function(_, bufnr)
-  --       local nmap = function(keys, func, desc)
-  --         if desc then
-  --           desc = 'LSP: ' .. desc
-  --         end
-  --
-  --         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  --       end
-  --
-  --       nmap('<leader>ra', vim.lsp.buf.rename, '[R]e[n]ame')
-  --       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  --
-  --       nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  --       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  --       nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  --       nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  --       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  --       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  --
-  --       -- See `:help K` for why this keymap
-  --       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  --       -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-  --
-  --       -- Lesser used LSP functionality
-  --       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  --       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  --       nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  --       nmap('<leader>wl', function()
-  --         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  --       end, '[W]orkspace [L]ist Folders')
-  --
-  --       -- Create a command `:Format` local to the LSP buffer
-  --       vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-  --         vim.lsp.buf.format()
-  --       end, { desc = 'Format current buffer with LSP' })
-  --     end
-  --
-  --     -- mason-lspconfig requires that these setup functions are called in this order
-  --     -- before setting up the servers.
-  --     require('mason').setup()
-  --     require('mason-lspconfig').setup()
-  --     -- Setup neovim lua configuration
-  --     require('neodev').setup()
-  --     local lsp = require('lspconfig')
-  --
-  --     local servers = {
-  --       html = {},
-  --       cssls = {},
-  --       clangd = {},
-  --       theme_check = {},
-  --       emmet_ls = {
-  --         filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'liquid' },
-  --         init_options = {
-  --           html = {
-  --             options = {
-  --               -- for possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#l79-l267
-  --               ["bem.enabled"] = true,
-  --             },
-  --           },
-  --         },
-  --
-  --       },
-  --       marksman = {},
-  --       tailwindcss = {},
-  --       tsserver = {
-  --         cmd = { "typescript-language-server", "--stdio" },
-  --         filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  --         init_options = {
-  --           hostInfo = "neovim"
-  --         },
-  --         root_dir = lsp.util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git"),
-  --         single_file_support = true,
-  --       },
-  --       -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-  --
-  --       lua_ls = {
-  --         Lua = {
-  --           workspace = { checkThirdParty = false },
-  --           telemetry = { enable = false },
-  --         },
-  --       },
-  --     }
-  --
-  --     local border = {
-  --       {"╭", "FloatBorder"},
-  --       {"─", "FloatBorder"},
-  --       {"╮", "FloatBorder"},
-  --       {"│", "FloatBorder"},
-  --       {"╯", "FloatBorder"},
-  --       {"─", "FloatBorder"},
-  --       {"╰", "FloatBorder"},
-  --       {"│", "FloatBorder"},
-  --     }
-  --     -- LSP settings (for overriding per client)
-  --     local handlers =  {
-  --       ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
-  --       ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
-  --     }
-  --
-  --     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-  --     local capabilities = vim.lsp.protocol.make_client_capabilities()
-  --     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-  --
-  --     -- Ensure the servers above are installed
-  --     local mason_lspconfig = require 'mason-lspconfig'
-  --
-  --     mason_lspconfig.setup {
-  --       ensure_installed = vim.tbl_keys(servers),
-  --     }
-  --
-  --     mason_lspconfig.setup_handlers {
-  --       function(server_name)
-  --         require('lspconfig')[server_name].setup {
-  --           capabilities = capabilities,
-  --           on_attach = on_attach,
-  --           settings = servers[server_name],
-  --           filetypes = (servers[server_name] or {}).filetypes,
-  --           handlers = handlers
-  --         }
-  --       end,
-  --     }
-  --
-  --   end
-  -- },
-
-
-  -- theprimeagean's lsp file (w/o the cmp). Mine LSP was not working. 
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -180,7 +30,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "rust_analyzer",
+          -- "rust_analyzer",
           "tsserver",
           "emmet_ls"
         },
@@ -191,6 +41,8 @@ return {
               capabilities = capabilities,
             }
           end,
+
+          ['rust_analyzer'] = function() end,
 
           ["lua_ls"] = function()
             local lspconfig = require("lspconfig")
@@ -220,6 +72,14 @@ return {
               },
             }
           end,
+          --[[ ["rust_analyzer"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.emmet_ls.setup({
+              capabilities = capabilities,
+              filetypes = {"rust"},
+              root_dir = 
+            })
+          end, ]]
         }
       })
 
